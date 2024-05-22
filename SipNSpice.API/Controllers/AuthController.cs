defd72c5aaac89b77992a152eaa2fcb7efa22ddd
Nathan Clive Rego <nathan.crego@outlook.com>
2024-05-22 13:32:56 +0530

    some corrections

--- SipNSpice.API/Controllers/AuthController.cs
+++ SipNSpice.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using SipNSpice.API.Models.DTO;
 using SipNSpice.API.Repositories.Interface;
@@ -25,45 +26,57 @@ namespace SipNSpice.API.Controllers
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequest)
         {
-            //Create IdentityUser Object
-            var user = new IdentityUser
+            if (ModelState.IsValid)
             {
-                UserName = registerRequest.Email?.Trim(),
-                Email = registerRequest.Email?.Trim()
-            };
-
-            //Create User in db
-            var identityResult = await userManager.CreateAsync(user, registerRequest.Password);
-            if(identityResult.Succeeded)
-            {
-                //Add the default role to the user (Reader)
-                identityResult = await userManager.AddToRoleAsync(user, "Reader");
-                if(identityResult.Succeeded)
+                //check if email exists
+                var emailExists = await userManager.FindByEmailAsync(registerRequest.Email);
+                if (emailExists != null)
                 {
-                    return Ok(identityResult);
+                    return BadRequest("Email already exists! Please register with the new email or login using the same");
                 }
-                else
+                //Create IdentityUser Object
+                var user = new IdentityUser
                 {
-                    if(identityResult.Errors.Any())
+                    UserName = registerRequest.Email?.Trim(),
+                    Email = registerRequest.Email?.Trim()
+                };
+
+                //Create User in db
+                var identityResult = await userManager.CreateAsync(user, registerRequest.Password);
+                if (identityResult.Succeeded)
+                {
+                    //Add the default role to the user (Reader)
+                    identityResult = await userManager.AddToRoleAsync(user, "Reader");
+                    if (identityResult.Succeeded)
                     {
-                        foreach(var error in identityResult.Errors)
+                        //return Ok(identityResult);
+                        //This code is used to login as soon as the user registers
+                        //Check the email entered
+                        var identityUser = await userManager.FindByEmailAsync(registerRequest.Email);
+                        if (identityUser != null)
                         {
-                            ModelState.AddModelError("", error.Description);
+                            //Check Password
+                            var checkPasswordResult = await userManager.CheckPasswordAsync(identityUser, registerRequest.Password);
+                            if (checkPasswordResult)
+                            {
+                                var roles = await userManager.GetRolesAsync(identityUser);
+
+                                //Once the initial checks are done. Create a token
+                                var jwtToken = tokenRepository.CreateJwtToken(identityUser, roles.ToList());
+                                var response = new LoginResponseDto()
+                                {
+                                    Email = registerRequest.Email,
+                                    Roles = roles.ToList(),
+                                    Token = jwtToken
+                                };
+                                return Ok(response);
+                            }
                         }
                     }
                 }
+         
             }
-            else
-            {
-                if(identityResult.Errors.Any())
-                {
-                    foreach(var error in identityResult.Errors)
-                    {
-                        ModelState.AddModelError("",error.Description);
-                    }
-                }
-            }
-            return ValidationProblem(ModelState);
+            return BadRequest("Please enter a valid email address");
         }
 
         //POST: {apibaseurl}/api/auth/login
@@ -92,8 +105,7 @@ namespace SipNSpice.API.Controllers
                     return Ok(response);
                 }
             }
-            ModelState.AddModelError("", "Email or Password is incorrect");
-            return ValidationProblem(ModelState);
+            return BadRequest("Email or password entered is incorrect!");
         }
     }
 }
