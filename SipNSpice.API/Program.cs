f83ad571d8350c202f2c17fd1875596a0fae9739
Nathan Clive Rego <nathan.crego@outlook.com>
2024-06-27 17:06:08 +0530

    Created appsettings prod and dev. Did necessary changes to the progrma.cs file.

--- SipNSpice.API/Program.cs
+++ SipNSpice.API/Program.cs
@@ -10,10 +10,16 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Configure environment-specific settings
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -39,7 +45,6 @@ builder.Services.AddScoped<IDrinkRepository, DrinkRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
 builder.Services.AddScoped<IRecipeImageRepository, RecipeImageRepository>();
 builder.Services.AddScoped<IDrinkImageRepository, DrinkImageRepository>();
-
 
 //Injecting Identity Core
 builder.Services.AddIdentityCore<IdentityUser>()
